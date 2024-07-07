52b0a940e0c388595cebda11a2c9b8d9c60d4f92
Duygu Tümer <69381839+duygutumer@users.noreply.github.com>
2024-07-08 02:19:23 +0300

    strongly connected done

--- Minimal-FSM-Generator/main.py
+++ Minimal-FSM-Generator/main.py
@@ -1,11 +1,11 @@
 from randomfsmgenerator import FSM
 
-num_of_states  = 3
+num_of_states  = 30
 num_of_inputs  = 2
 num_of_outputs = 2
 
 myfsm = FSM(num_of_states, num_of_inputs, num_of_outputs)
-myfsm.generateMinimal()
+myfsm.generateStronglyConnectedMinimal()
 myfsm.output_for_ads("../examples/ads_example.dot")
-myfsm.show()
+# myfsm.show()
 # myfsm.draw()
--- Minimal-FSM-Generator/randomfsmgenerator.py
+++ Minimal-FSM-Generator/randomfsmgenerator.py
@@ -3,6 +3,7 @@
 global GraphvizImportSuccessful
 
 import random
+import tarjan
 
 from collections import deque
 
@@ -86,6 +87,100 @@ class FSM:
         while not self.isSurelyMinimal(): #if minimality check gives false
             self.clear()
             self.generate()
+            
+    def generateStronglyConnectedMinimal(self):
+        
+        self.generate()
+        
+        adj = self.create_adj_list()
+        # self.show(True)
+        scc = tarjan.tarjan(adj)
+        scc = sorted(scc, key=len, reverse=True)
+        print("scc ->", scc)
+        
+        loops = 0
+        operations = 0
+
+        while len(scc) != 1:
+            print("Multiple groups, trying connections")
+            loops += 1
+            for i in range(1, len(scc)):
+                rt = self.get_reverse_transitions()
+                group_node = scc[i][0]
+                
+                # Check reverse transitions of this node to see if there are any incoming transitions
+                incoming = False
+                for i in rt[group_node]:
+                    if i != group_node:
+                        incoming = True
+                
+                if not incoming:
+                    operations += 1
+                    # Choose a random node to connect to this one
+                    node_index = random.choice(scc[0])
+                    # Choose a random transition
+                    transition_index = random.randint(0, self.numOfInputs-1)
+                    
+                    # Connect it back here
+                    self.nodes[node_index].transitions[transition_index] = (self.nodes[group_node], self.nodes[node_index].transitions[transition_index][1])
+                    print("i Connected the", transition_index, "transition of", node_index, "to", group_node)
+                    
+                outgoing = False
+                for transition in self.nodes[group_node].transitions:
+                    if transition[0].index != group_node:
+                        outgoing = True
+                
+                if not outgoing:
+                    operations += 1
+                    # Choose a random node to connect to this one
+                    node_index = random.choice(scc[0])
+                    # Choose a random transition
+                    transition_index = random.randint(0, self.numOfInputs-1)
+                    
+                    self.nodes[group_node].transitions[transition_index] = (self.nodes[node_index], self.nodes[group_node].transitions[transition_index][1])
+                    print("o Connected the", transition_index, "transition of", group_node, "to", node_index)
+                
+                if incoming and outgoing:
+                    operations += 1
+                    if random.randint(1, 2) == 1:
+                        node_index = random.choice(scc[0])
+                        # Choose a random transition
+                        transition_index = random.randint(0, self.numOfInputs-1)
+                        
+                        # Connect it back here
+                        self.nodes[node_index].transitions[transition_index] = (self.nodes[group_node], self.nodes[node_index].transitions[transition_index][1])
+                        print("ii Connected the", transition_index, "transition of", node_index, "to", group_node)
+                    else:
+                        # Choose a random node to connect to this one
+                        node_index = random.choice(scc[0])
+                        # Choose a random transition
+                        transition_index = random.randint(0, self.numOfInputs-1)
+                        
+                        self.nodes[group_node].transitions[transition_index] = (self.nodes[node_index], self.nodes[group_node].transitions[transition_index][1])
+                        print("oo Connected the", transition_index, "transition of", group_node, "to", node_index)
+                    
+            adj = self.create_adj_list()
+            # self.show()
+            scc = tarjan.tarjan(adj)
+            scc = sorted(scc, key=len, reverse=True)
+            print("scc ->", scc)
+
+        print("Done!")
+        print("Loops ->", loops, " Operations ->", operations)
+        print("Is minimal =>", self.isSurelyMinimal())
+        
+        while not self.isSurelyMinimal():
+            self.clear()
+            self.generateStronglyConnectedMinimal()
+    
+    def create_adj_list(self):
+        adj_list = {}
+        
+        for node in self.nodes:
+            adj_list[node.index] = []
+            for transition in node.transitions:
+                adj_list[node.index].append(transition[0].index)
+        return adj_list
 
     def generateRandomTrace(self, length=10, startNode=-1):
         # Generates an input trace
@@ -410,12 +505,7 @@ class FSM:
                         node.transitions[i] = (random.choice(self.nodes), random.randint(0, self.numOfOutputs - 1))
                 
                 
-    def generateStronglyConnectedMinimal(self):
-        self.generateStronglyConnected()
-
-        while not self.isSurelyMinimal():
-            self.clear()
-            self.generateStronglyConnected()  
+    
             
 if __name__ == "__main__":
     
