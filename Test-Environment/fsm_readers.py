bedcb1b51e7c1b31f28e835f9739d007034f5170
Digdem Yildiz <digdemyildiz19@ku.edu.tr>
2024-08-21 13:56:18 +0300

    added transition tour reader

--- Test-Environment/fsm_readers.py
+++ Test-Environment/fsm_readers.py
@@ -31,3 +31,11 @@ def read_faulty_fsm(file_path: str):
                 return string
         edges = [tuple(map(read_transitions, f.readline().strip().split(","))) for _ in range(transition_num)]
     return  fault_idx, state_num, transition_num, input_num, output_num, seed, edges
+
+def read_transitions_tour(file_path: str):
+    with open(file_path, 'r') as f:
+        f.readline()
+        f.readline()
+        input_seq = f.readline().strip().split(",")
+        output_seq = f.readline().strip().split(",")
+    return input_seq, output_seq
--- Test-Environment/test.py
+++ Test-Environment/test.py
@@ -2,7 +2,9 @@ import fsm_readers
 
 fsm_path = "../examples/PURE2024/test_machines/16_states/test_machine_16_states_17_seed.csv"
 faulty_fsm_path = "../examples/PURE2024/faulty_test_machines/16_states/faulty_test_machine_16_states_17_seed.csv"
+transition_tour_path = "../examples/PURE2024/transition_tours/16_states/transition_tour_16_states_17_seed.csv"
 
 
 state_num, transition_num, input_num, output_num, seed, transitions = fsm_readers.read_fsm(fsm_path)
 fault_idx, state_num, transition_num, input_num, output_num, seed, transitions = fsm_readers.read_faulty_fsm(faulty_fsm_path)
+transition_tour_inp, transition_tour_outp = fsm_readers.read_transitions_tour(transition_tour_path)
