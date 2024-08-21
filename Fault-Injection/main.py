b2179a742d29d47734c8580df8fa10c110439e9a
Digdem Yildiz <digdemyildiz19@ku.edu.tr>
2024-08-21 12:34:17 +0300

    faulty generation is comleted and faulty machines are generated

--- Fault-Injection/main.py
+++ Fault-Injection/main.py
@@ -9,7 +9,7 @@ else:
 
 fsm_dir = "../examples/PURE2024/test_machines/" 
 
-fault_injection_dir = "../examples/PURE2024/fault_injections/" 
+fault_injection_dir = "../examples/PURE2024/faulty_test_machines/" 
 
 for filename in os.listdir(fsm_dir + state_num + "_states/"):
 
@@ -17,5 +17,8 @@ for filename in os.listdir(fsm_dir + state_num + "_states/"):
         fsm_path = fsm_dir + str(state_num) + "_states/"+ filename
         state_num, transition_num, input_num, output_num, seed, transitions = fi.read_fsm(fsm_path)
      
-        injected_fault, injection_idx = fi.inject_fault(state_num, transition_num, seed, transitions)
+        faulty_transitions, injection_idx = fi.inject_fault(state_num, transition_num, seed, transitions)
+
+        faulty_fsm_path = fault_injection_dir + str(state_num) + "_states/faulty_" +  filename
+        fi.write_faulty_fsm(faulty_fsm_path, injection_idx, state_num, transition_num, input_num, output_num, seed, faulty_transitions)
         
