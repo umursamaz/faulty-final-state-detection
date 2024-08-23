18f0e968cb804b50ea9643a1108c970cebfcb1ac
Digdem Yildiz <digdemyildiz19@ku.edu.tr>
2024-08-23 14:16:49 +0300

    small editing

--- Test-Environment/test.py
+++ Test-Environment/test.py
@@ -21,24 +21,23 @@ def test(fsm_path, faulty_fsm_path, transition_tour_path):
 
 
 
+if __name__ == "__main__":
+    fsm_path = "../examples/PURE2024/test_machines/128_states/test_machine_128_states_56_seed.csv"
+    faulty_fsm_path = "../examples/PURE2024/faulty_test_machines/128_states/faulty_test_machine_128_states_56_seed.csv"
+    transition_tour_path = "../examples/PURE2024/transition_tours/128_states/transition_tour_128_states_56_seed.csv"
+        
+    state_num, transition_num, input_num, output_num, seed, transitions = fsm_readers.read_fsm(fsm_path)
+    fault_idx, state_num, transition_num, input_num, output_num, seed, faulty_transitions = fsm_readers.read_faulty_fsm(faulty_fsm_path)
+    transition_tour_inp, transition_tour_outp = fsm_readers.read_transitions_tour(transition_tour_path)
 
+    specification_fsm = FSM(state_num, transition_num, input_num, output_num, seed, transitions)
+    iut_fsm = FSM(state_num, transition_num, input_num, output_num, seed, faulty_transitions, fault_idx)
 
-fsm_path = "../examples/PURE2024/test_machines/128_states/test_machine_128_states_56_seed.csv"
-faulty_fsm_path = "../examples/PURE2024/faulty_test_machines/128_states/faulty_test_machine_128_states_56_seed.csv"
-transition_tour_path = "../examples/PURE2024/transition_tours/128_states/transition_tour_128_states_56_seed.csv"
-    
-state_num, transition_num, input_num, output_num, seed, transitions = fsm_readers.read_fsm(fsm_path)
-fault_idx, state_num, transition_num, input_num, output_num, seed, faulty_transitions = fsm_readers.read_faulty_fsm(faulty_fsm_path)
-transition_tour_inp, transition_tour_outp = fsm_readers.read_transitions_tour(transition_tour_path)
+    output_expected, fault_inp_idx_s = specification_fsm.apply(transition_tour_inp)
+    output_experiment, fault_inp_idx_i = iut_fsm.apply(transition_tour_inp)
 
-specification_fsm = FSM(state_num, transition_num, input_num, output_num, seed, transitions)
-iut_fsm = FSM(state_num, transition_num, input_num, output_num, seed, faulty_transitions, fault_idx)
-
-output_expected, fault_inp_idx_s = specification_fsm.apply(transition_tour_inp)
-output_experiment, fault_inp_idx_i = iut_fsm.apply(transition_tour_inp)
-
-if output_expected != output_experiment:
-    print(fault_inp_idx_i)
-    print(find_suspected_states(transition_tour_inp, output_experiment, fault_inp_idx_i, specification_fsm, iut_fsm))
+    if output_expected != output_experiment:
+        print(fault_inp_idx_i)
+        print(find_suspected_states(transition_tour_inp, output_experiment, fault_inp_idx_i, specification_fsm, iut_fsm))
 
 
